af53f1b4c22fa258826d70404cb076eea76f685a
Yvonne K <yvonneking017@gmail.com>
2020-03-30 17:25:04 -0500

    wrangle file added

---
+++ regression/wrangle.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from env import get_db_url
+
+def wrangle_telco():
+    query = """SELECT customer_id,
+tenure,
+monthly_charges,
+total_charges
+FROM customers
+WHERE contract_type_id = 3;"""
+    url = get_db_url("telco_churn")
+    telco = pd.read_sql(query, url)
+    telco.total_charges = telco.total_charges.str.strip()
+    telco = telco.replace("", np.nan)
+    telco = telco.dropna()
+    telco.total_charges = telco.total_charges.astype("float")
+    return telco
