b329fa45781a2ebbebf3fca1e53c450fbb17e7a4
Yvonne K <yvonneking017@gmail.com>
2020-04-14 10:26:40 -0500

    tweaked function to get titanic df

--- classification/acquire.py
+++ classification/acquire.py
@@ -6,10 +6,10 @@ def get_db_url(db):
 
 
 def get_titanic_data():
-    sheet_url = "https://docs.google.com/spreadsheets/d/1PmmRUXgmQ6oO9fLORG4oeMLe_mzIWlFAkJKA2cwLOLg/edit#gid=935554057"
-    csv_export_url = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
-    df_titanic = pd.read_csv(csv_export_url)
-    return df_titanic
+    url= get_db_url("titanic_db")
+    query = "SELECT * FROM passengers"
+    df = pd.read_sql(query, url)
+    return df
 
 
 def get_iris_data():
