4c33f6289583e22ac3c19f82fc83c22a031dcff4
Yvonne K <yvonneking017@gmail.com>
2020-04-14 09:01:57 -0500

    completed get_iris function

--- classification/acquire.py
+++ classification/acquire.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import env
+
+def get_db_url(db):
+    return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{db}'
 
 
 def get_titanic_data():
@@ -6,3 +10,12 @@ def get_titanic_data():
     csv_export_url = sheet_url.replace('/edit#gid=', '/export?format=csv&gid=')
     df_titanic = pd.read_csv(csv_export_url)
     return df_titanic
+
+
+def get_iris_data():
+    url = get_db_url("iris_db")
+    query = """SELECT * 
+    FROM measurements
+    JOIN species USING(species_id);"""
+    df = pd.read_sql(query, url)
+    return df
