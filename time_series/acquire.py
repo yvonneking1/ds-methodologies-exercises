fb28c6e1fba6f817140e200114d5dfb2302dfa0a
Yvonne K <yvonneking017@gmail.com>
2020-05-13 08:58:47 -0500

    tweaked code

--- time_series/acquire.py
+++ time_series/acquire.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 #to make HTTP requests
 import requests
@@ -62,5 +63,32 @@ def get_all_data():
     
     df = sales.merge(items, on="item_id").merge(stores, on="store_id")
     
-    return df
+
+    #checks to see if the dataframe is a CSV
+    if os.path.isfile('big_df.csv'):
+        df = pd.read_csv('big_df.csv', parse_dates=True, index_col='sale_date')
+        return df
+    else:
+        # convert sale_date to DateTime Index
+        df['sale_date'] = pd.to_datetime(df.sale_date)
+        df = df.sort_index()# convert sale_date to DateTime Index
+        df['sale_date'] = pd.to_datetime(df.sale_date)
+        df = df.sort_index()
+        
+        #write to csv for future use
+        df.to_csv('big_df.csv')
+        return df    
+
     
+def german_energy_csv():
+    """
+    This function returns a df with a datetime index
+    using the opsd_germany url/csv.
+    """
+    if os.path.isfile('german_energy.csv'):
+        df = pd.read_csv('german_energy.csv')
+    else:
+        url = 'https://raw.githubusercontent.com/jenfly/opsd/master/opsd_germany_daily.csv'
+        df = pd.read_csv(url)
+        df.to_csv('german_energy.csv')
+    return df  
