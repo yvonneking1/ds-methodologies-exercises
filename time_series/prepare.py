02f4a78b5c5d22123c215cdefbbeb076af94b9f7
Yvonne K <yvonneking017@gmail.com>
2020-05-13 16:03:22 -0500

    added histogram function

--- time_series/prepare.py
+++ time_series/prepare.py
@@ -10,6 +10,19 @@ import requests
 import acquire
 
 
+def numeric_hists(df, bins=20):
+    """
+    Function to take in a DataFrame, bins default 20,
+    select only numeric dtypes, and
+    display histograms for each numeric column
+    """
+    plt.rc('figure', figsize=(11, 9))
+    plt.rc('font', size=13)
+    num_df = df.select_dtypes(include=np.number)
+    num_df.hist(bins=bins, color='thistle')
+    plt.suptitle('Numeric Column Distributions')
+    plt.show()
+    
 
 def prep_store_data():
     #get the data from acquire file
@@ -17,13 +30,21 @@ def prep_store_data():
     
     #Add a 'month' and 'day of week' column
     store["month"] = store.index.month_name()
-    store["day_of_week"] = store.index.day_name()
+    store["weekday"] = store.index.day_name()
     
     #Add sales total column from sale_amount and item_price
     store["sales_total"] = store.sale_amount * store.item_price
     
     #new column that is the result of the current sales - the previous days sales.
     store["sales_diff(1)"] = store.sales_total.diff(1)
+    
+    # Change dtypes of numeric columns to object and category
+    store = (store.astype({'sale_id': object, 'store_id': object, 
+                     'store_zipcode': object, 'item_id': object, 
+                     'item_upc12': object, 'item_upc14': object, 
+                     'month': 'category', 'weekday': 'category'}))
+    
+    numeric_hists(store)
     
     return store
 
@@ -41,5 +62,8 @@ def prep_german_energy():
     #Add a month and a year column
     ops["month"] = ops.index.month_name()
     ops["year"] = ops.index.year
+   
+    
+    numeric_hists(ops)
     
     return ops
